b339bd6784887704d9b36607dc3dacb5dc75df03
Weber Liu <weber.liu@gmail.com>
2019-05-05 23:21:12 +0800

    加密和解密分别支持输出字符串或者文件

--- test/test.js
+++ test/test.js
@@ -3,99 +3,87 @@ var path = require('path')
 var rimraf = require('rimraf')
 var imageEncrypt = require('../lib')
 
+var outputPath = path.resolve(__dirname, 'output')
 var filePath = path.resolve(__dirname, 'src', 'wolf.jpg')
-var encryptedFilePath = path.resolve(__dirname, 'src', 'image.ept')
-var output = path.resolve(__dirname, 'output')
+var encryptedFilePath = path.resolve(__dirname, 'src', 'wolf.ept')
 var key = 'so78e/GuQ1y/gQ9wtF2z/EYNmhnaGmtnWv2y45viVFM='
 
-describe('async encrypt image', () => {
-  beforeAll(() => {
-    rimraf.sync(output)
-    fs.mkdirSync(output)
+describe('encrypt image', () => {
+  test('async to string', () => {
+    imageEncrypt
+      .encrypt({ filePath })
+      .then(img => {
+        expect(img).toHaveProperty('data')
+        expect(img).toHaveProperty('key')
+      })
+      .catch(err => console.error(err))
   })
 
-  test('do not spcecified destination directory', () => {
+  test('sync to string', () => {
+    var img = imageEncrypt.encryptSync({ filePath })
+    expect(img).toHaveProperty('data')
+    expect(img).toHaveProperty('key')
+  })
+
+  test('async to file', () => {
     imageEncrypt
-      .encrypt({ filePath, key })
+      .encryptTo({ filePath, destPath: outputPath })
       .then(res => expect(fs.existsSync(res.destPath)).toBeTruthy())
       .catch(err => console.error(err))
   })
 
-  // test('specified destination directory', () => {
-  //   imageEncrypt
-  //     .encrypt({ filePath, destDir: output })
-  //     .then(res => expect(fs.existsSync(res.destPath)).toBeTruthy())
-  //     .catch(err => console.error(err))
-  // })
+  test('sync to file', () => {
+    var res = imageEncrypt.encryptToSync({ filePath, destPath: outputPath })
+    expect(fs.existsSync(res.destPath)).toBeTruthy()
+  })
+
+  test('specified secret key', () => {
+    var res = imageEncrypt.encryptSync({ filePath, key })
+    expect(res.key).toEqual(key)
+  })
 })
 
-// describe('sync encrypt image', () => {
-//   beforeAll(() => {
-//     rimraf.sync(output)
-//     fs.mkdirSync(output)
-//   })
+describe('decrypt image', () => {
+  test('async to base64 string', () => {
+    imageEncrypt
+      .decrypt({ filePath: encryptedFilePath, key })
+      .then(b64 => expect( b64.substr(0, 10)).toEqual('data:image'))
+  })
 
-//   test('do not spcecified destination directory', () => {
-//     var res = imageEncrypt.encryptSync({ filePath, key })
-//     expect(fs.existsSync(res.destPath)).toBeTruthy()
-//   })
+  test('sync to base64 string', () => {
+    var b64 = imageEncrypt.decryptSync({ filePath: encryptedFilePath, key })
+    var prefix = b64.substr(0, 10)
+    expect(prefix).toEqual('data:image')
+  })
 
-//   test('spcecified destination directory', () => {
-//     var res = imageEncrypt.encryptSync({ filePath, destDir: output })
-//     expect(fs.existsSync(res.destPath)).toBeTruthy()
-//   })
-// })
+  test('async to image file', () => {
+    imageEncrypt
+      .decryptTo({
+        filePath: encryptedFilePath,
+        key,
+        destPath: outputPath
+      })
+      .then(res => {
+        expect(fs.existsSync(res)).toBeTruthy()
+      })
+  })
 
-// describe('async decrypt image', () => {
-//   beforeAll(() => {
-//     rimraf.sync(output)
-//     fs.mkdirSync(output)
-//   })
+  test('sync to image file', () => {
+    var img = imageEncrypt.decryptToSync({
+      filePath: encryptedFilePath,
+      key,
+      destPath: outputPath
+    })
 
-//   test('output base64 string', () => {
-//     imageEncrypt
-//       .decrypt({
-//         filePath: encryptedFilePath,
-//         key
-//       })
-//       .then(b64 => {
-//         var prefix = b64.substr(0, 10)
-//         expect(prefix).toEqual('data:image')
-//       })
-//   })
+    expect(fs.existsSync(img)).toBeTruthy()
+  })
 
-//   test('write image file', () => {
-//     imageEncrypt
-//       .decrypt({
-//         filePath: encryptedFilePath,
-//         key,
-//         destDir: output
-//       })
-//       .then(res => expect(fs.existsSync(res)).toBeTruthy())
-//   })
-// })
-
-describe('sync decrypt file', () => {
-  // beforeAll(() => {
-  //   rimraf.sync(output)
-  //   fs.mkdirSync(output)
-  // })
-
-  // test('output base64 string', () => {
-  //   var b64 = imageEncrypt.decryptSync({
-  //     filePath: encryptedFilePath,
-  //     key
-  //   })
-  //   var prefix = b64.substr(0, 10)
-  //   expect(prefix).toEqual('data:image')
-  // })
-
-  // test('output image file', () => {
-  //   var img = imageEncrypt.decryptSync({
-  //     filePath: encryptedFilePath,
-  //     key,
-  //     destDir: output
-  //   })
-  //   expect(fs.existsSync(img)).toBeTruthy()
-  // })
+  test('incorrect secret key', () => {
+    expect(() => {
+      imageEncrypt.decryptSync({
+        filePath: encryptedFilePath,
+        key: 'so78e/Gup5y/gQ9wtF2z/EYNmhnaGmtnWv2y45viVFM='
+      })
+    }).toThrow()
+  })
 })
